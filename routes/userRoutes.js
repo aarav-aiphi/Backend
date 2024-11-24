5151496107056a4b50fccb3478abe00c79d142b7
aarav-aiphi <aarav.v@aiphi.ai>
2024-11-24 18:46:23 +0530

    fg

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -87,7 +87,7 @@ router.post('/signup', async (req, res) => {
     // Check if the user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists with this email' });
+      return res.status(201).json({ message: 'User already exists with this email' });
     }
 
     // Hash password before saving
@@ -108,7 +108,7 @@ router.post('/signup', async (req, res) => {
     const savedUser = await newUser.save();
 
     // Generate token and set it in cookies
-    generateToken(savedUser, res, 201, 'User registered successfully');
+    generateToken(savedUser, res, 200, 'User registered successfully');
   } catch (error) {
     console.error('Error signing up user:', error);
     res.status(500).json({ message: 'Error signing up user', error });
