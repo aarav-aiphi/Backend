75bdbd5fae6470cd3412b705087f6b9d8c1337a6
aarav-aiphi <aarav.v@aiphi.ai>
2024-12-30 19:24:18 +0530

    f

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ import User from '../models/User.js'; // Adjust the path as needed
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/api/users/auth/google/callback'
+  callbackURL: 'https://backend-1-sval.onrender.com/api/users/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
